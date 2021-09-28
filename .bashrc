1436e714983c229b1bb461338950968dbd68f43c
DelightedCat <git@delightedcat.net>
2021-09-28 21:38:58 +0200

    Delete .bashrc, move .nvimrc and, rename avatar.png and update install script

--- .bashrc
+++
@@ -1,5 +0,0 @@
-
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-
-ssh-add ~/.ssh/{sourcehut,github} >/dev/null 2>&1
-/usr/bin/setxkbmap -option "caps:escape"
--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 #
 # A simple script that installs dotfiles to the current system.
 #
+
+# Copy over all backgrounds, icons and themes.
 cp -r backgrounds/* /usr/share/backgrounds
 cp -r icons/* /usr/share/icons
 cp -r themes/* /usr/share/themes
 
-cp avatar.png ~/.face
+# Copy the display manager profile image over.
+cp .face ~/.face
+
+# Put the Neovim configuration file in place.
+mkdir -p ~/.config/nvim
+cp -r nvim/* ~/.config/nvim
