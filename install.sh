aeda837acbe2421c89c4e8fa85c9613a4b06440a
DelightedCat <git@delightedcat.net>
2021-12-01 23:23:44 +0100

    Add theme and icons to the repository as submodules and add to install script

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@
 # Author: DelightedCat <me@delightedcat.net>
 #
 
-# The absolute path to the cp binary file.
+# The absolute path to the git and cp binaries.
+GIT_BIN=/usr/bin/git
 CP_BIN=/bin/cp
+
+# Ensure we got the latest submodules pulled in.
+${GIT_BIN} submodule update --init --recursive
 
 # Copy all global dotfiles to their global destinations.
 ${CP_BIN} -rv ./backgrounds/* /usr/share/backgrounds/
+${CP_BIN} -rv ./themes/* /usr/share/themes/
+${CP_BIN} -rv ./icons/* /usr/share/icons/
 ${CP_BIN} -rv ./xfce4/terminal/colorschemes/* /usr/share/xfce4/terminal/colorschemes/
 
 # Copy all user dotfiles to their destinations.
